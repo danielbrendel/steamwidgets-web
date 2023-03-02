8eee40df2acad40fd69e3ba563af8bdde85fba9a
Daniel Brendel <dbrendel1988@gmail.com>
2023-03-02 13:45:02 +0100

    Error 404 info

--- app/controller/error404.php
+++ app/controller/error404.php
@@ -12,14 +12,6 @@ class Error404Controller extends BaseController {
 	 */
 	public function index($request)
 	{
-		//Add a log line
-		addLog(ASATRU_LOG_INFO, "Error 404");
-
-		//Generate the 404 view
-		$v = new Asatru\View\ViewHandler();
-		$v->setLayout('layout') //The layout file. Will be \app\views\layout.php
-			->setYield('yield', 'error/404'); //The index yield. Will be \app\views\error\404.php
-		
-		return $v; //Pass the object to the engine
+		return parent::view(['content', 'error/404']);
 	}
 }
--- app/views/error/404.php
+++ app/views/error/404.php
@@ -1,17 +1,7 @@
 <!-- Error 404 yield file -->
 
-<div class="outer">
-	<div class="inner">
-        <div class="title">
-            <h1>Error 404</h1>
-        </div>
+<div class="content-section">
+    <h1>Error 404</h1>
 
-        <div class="text">
-            <p>The requested resource <?= $_SERVER['REQUEST_URI']; ?> was not found on the server.</p>
-        </div>
-
-        <div class="links">
-            <button type="button" class="button btn-col-contact" onclick="location.href = '{{ url('/') }}';">Go home</button>
-        </div>
-    </div>
+    <p>The requested resource <?= $_SERVER['REQUEST_URI']; ?> was not found on the server.</p>
 </div>
