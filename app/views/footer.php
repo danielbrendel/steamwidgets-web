055d4c9d0248727d34c98e0061b56f268e44b5d3
Daniel Brendel <dbrendel1988@gmail.com>
2023-03-02 13:55:05 +0100

    No Twitter

--- app/views/footer.php
+++ app/views/footer.php
@@ -4,7 +4,7 @@
         <div class="column is-4">
             <div class="footer-frame">
                 <div class="footer-content">
-                    &copy; {{ date('Y') }} by {{ env('APP_AUTHOR') }} | <a href="{{ env('APP_LINK_GITHUB') }}" title="GitHub"><i class="fab fa-github"></i></a>&nbsp;&nbsp;<a href="{{ env('APP_LINK_TWITTER') }}" title="Twitter"><i class="fab fa-twitter"></i></a>&nbsp;&nbsp;<a href="{{ env('APP_LINK_STEAM') }}" title="Steam"><i class="fab fa-steam"></i></a>
+                    &copy; {{ date('Y') }} by {{ env('APP_AUTHOR') }} | <a href="{{ env('APP_LINK_GITHUB') }}" title="GitHub"><i class="fab fa-github"></i></a>&nbsp;&nbsp;<a href="{{ env('APP_LINK_STEAM') }}" title="Steam"><i class="fab fa-steam"></i></a>
                 </div>
             </div>
         </div>
