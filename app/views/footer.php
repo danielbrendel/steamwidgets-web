d9e021f4ed875dd75b50bb9d0f52e75114b4e81b
Daniel Brendel <dbrendel1988@gmail.com>
2023-03-22 10:23:04 +0100

    Legal statement

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,13 +1,14 @@
 <div class="footer">
     <div class="columns">
-        <div class="column is-4"></div>
-        <div class="column is-4">
+        <div class="column is-2"></div>
+        <div class="column is-8">
             <div class="footer-frame">
                 <div class="footer-content">
-                    &copy; {{ date('Y') }} by {{ env('APP_AUTHOR') }} | <a href="{{ env('APP_LINK_GITHUB') }}" title="GitHub"><i class="fab fa-github"></i></a>&nbsp;&nbsp;<a href="{{ env('APP_LINK_STEAM') }}" title="Steam"><i class="fab fa-steam"></i></a>
+                    <div>&copy; {{ date('Y') }} by {{ env('APP_AUTHOR') }} | <a href="{{ env('APP_LINK_GITHUB') }}" title="GitHub"><i class="fab fa-github"></i></a>&nbsp;&nbsp;<a href="{{ env('APP_LINK_STEAM') }}" title="Steam"><i class="fab fa-steam"></i></a></div>
+                    <div>{{ env('APP_NAME') }} is not affiliated with Valve or Steam</div>
                 </div>
             </div>
         </div>
-        <div class="column is-4"></div>
+        <div class="column is-2"></div>
     </div>
 </div>
