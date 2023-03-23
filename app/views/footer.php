029e47c3bc057f1cdcdf4ce930ff0c9b61802651
Daniel Brendel <dbrendel1988@gmail.com>
2023-03-23 01:02:03 +0100

    Get from env

--- app/views/footer.php
+++ app/views/footer.php
@@ -5,7 +5,7 @@
             <div class="footer-frame">
                 <div class="footer-content">
                     <div>&copy; {{ date('Y') }} by {{ env('APP_AUTHOR') }} | <a href="{{ env('APP_LINK_GITHUB') }}" title="GitHub"><i class="fab fa-github"></i></a>&nbsp;&nbsp;<a href="{{ env('APP_LINK_STEAM') }}" title="Steam"><i class="fab fa-steam"></i></a></div>
-                    <div>{{ env('APP_NAME') }} is not affiliated with Valve or Steam</div>
+                    <div>{{ env('APP_FOOTER_NOTICE') }}</div>
                 </div>
             </div>
         </div>
