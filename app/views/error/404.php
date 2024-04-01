bfba859ac7a87c9553a2dd95953f3adee190eaf2
Sophia Atkinson <sophialul@protonmail.com>
2024-03-31 17:42:59 -0700

    fix 404 page

--- app/views/error/404.php
+++ app/views/error/404.php
@@ -1,5 +1,6 @@
 <div class="content-section">
-    <h1>Error 404</h1>
-
-    <p>The requested resource <?= $_SERVER['REQUEST_URI']; ?> was not found on the server.</p>
+<br>    
+<br>
+<h1>Error 404</h1>
+<p>The requested resource <?= $_SERVER['REQUEST_URI']; ?> was not found on the server.</p>
 </div>
